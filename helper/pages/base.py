36ddde22879273a27da1b2a46a5cfb55ec7a2132
aabuiluk <andrii.a.builuk@gmail.com>
2024-02-12 21:57:39 +0200

    Fix

--- helper/pages/base.py
+++ helper/pages/base.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from helper.xpath import base
+#1
 
 
 class BasePage:
