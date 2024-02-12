6faff0c0956bde7c2ce22bfbd0cbfc880b644dae
Famik@Famik-PC <mblshata@gmail.com>
2024-02-12 20:30:44 +0200

    XPATH police_accessories

--- helper/xpath/base.py
+++ helper/xpath/base.py
@@ -5,3 +5,4 @@ menu = '//div[contains(@class,"mainmenublock")]//div[contains(@class,"mainmenu")
 tactic_shoes = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/obuv")]'
 guard_clothes = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/forma-ohranyi")]'
 police_uniform = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/dlya-politsii")]'
+open_police_accessories = '//div[@class="col-xs-12"]/div/ul/li//a[contains(@href,"aksessuaryi-dlya-politsii")]/p[contains(@style,"height:")]'
