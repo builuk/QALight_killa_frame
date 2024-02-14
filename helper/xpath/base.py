ba427fdc63e80eee2b95593101788d4c967661fb
Anastasiia <stasy.contact@gmail.com>
2024-02-14 13:40:00 +0000

    button_tourist_equipment added

--- helper/xpath/base.py
+++ helper/xpath/base.py
@@ -5,3 +5,4 @@ menu = '//div[contains(@class,"mainmenublock")]//div[contains(@class,"mainmenu")
 tactic_shoes = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/obuv")]'
 guard_clothes = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/forma-ohranyi")]'
 police_uniform = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/dlya-politsii")]'
+button_tourist_equipment = "//div[contains(@class, 'mainmenu ')]/ul[contains(@class,'list-unstyled')]/li/a[contains(@href,'/katalog/pohodnoe-snaryazhenie')]"
