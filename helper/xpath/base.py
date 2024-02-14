b3c7d5365fce1d9e7ace23f24ad28ac0378e598f
Anastasiia <stasy.contact@gmail.com>
2024-02-14 16:38:55 +0000

    merged changes

--- helper/xpath/base.py
+++ helper/xpath/base.py
@@ -5,4 +5,7 @@ menu = '//div[contains(@class,"mainmenublock")]//div[contains(@class,"mainmenu")
 tactic_shoes = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/obuv")]'
 guard_clothes = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/forma-ohranyi")]'
 police_uniform = '//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/dlya-politsii")]'
+open_police_accessories = '//div[@class="col-xs-12"]/div/ul/li//a[contains(@href,"aksessuaryi-dlya-politsii")]/p[contains(@style,"height:")]'
+sneakers = '//div/a[contains(@href,"katalog/obuv/obuvv/krossovki")]/img'
+fishing_hunting ='//div[contains(@class,"mainmenu")]/ul[@class="list-unstyled"]/li/a[contains(@href,"katalog/ryibalka-i-ohota/")]'
 button_tourist_equipment = "//div[contains(@class, 'mainmenu ')]/ul[contains(@class,'list-unstyled')]/li/a[contains(@href,'/katalog/pohodnoe-snaryazhenie')]"
