35102dfb4fdde2ea2a13214743a2bc954029d1e7
aabuiluk <andrii.a.builuk@gmail.com>
2024-02-17 11:27:52 +0200

    Fix test

--- test/test_builuk.py
+++ test/test_builuk.py
@@ -16,29 +16,12 @@ def browser():
     yield driver
     driver.quit()
 
-# @pytest.fixture
-# def browser_firefox():
-#     option = Options()
-#     option.add_argument("--start-maximized")
-#     prefs = {"profile.default_content_setting_values.notifications": 1}
-#     option.add_experimental_option("prefs", prefs)
-#     driver = webdriver.Firefox(options=option)
-#     yield driver
-#     driver.quit()
-
 def test_check_sneakers_image_positive(browser):
     tactic = base.TacticShoes(browser)
     tactic.open()
     tactic.open_menu().click()
     tactic.open_tactic_shoes().click()
     assert tactic.sneakers_image().get_attribute('src') == data.sneakers_image
-
-# def test_check_sneakers_image_positive_f(browser_firefox):
-#     tactic = base.TacticShoes(browser_firefox)
-#     tactic.open()
-#     tactic.open_menu().click()
-#     tactic.open_tactic_shoes().click()
-#     assert tactic.sneakers_image().get_attribute('src') == data.sneakers_image
 
 def test_title_positive(browser):
     tactic = base.BasePage(browser)
