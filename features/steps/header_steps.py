5fdae4981a7e75dce0da80e25480872ce60b9828
aabuiluk <andrii.a.builuk@gmail.com>
2024-02-27 16:10:58 +0000

    Check New Config

--- features/steps/header_steps.py
+++ features/steps/header_steps.py
@@ -12,10 +12,6 @@ def open_homepage(context):
     option = Options()
     prefs = {"profile.default_content_setting_values.notifications": 1}
     option.add_experimental_option("prefs", prefs)
-    # chrome_driver_path = '/usr/local/bin/chromedriver'
-    # option.binary_location = "/usr/bin/google-chrome"
-    # option.add_argument("executable_path=" + chrome_driver_path)
-
     context.browser = webdriver.Chrome(options=option)
     context.base_page = pages.BasePage(context.browser)
     context.base_page.open()
